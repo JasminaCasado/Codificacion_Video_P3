9d2c0742635dedbecb18a4d95b099822e35c3527
JasminaCasado <75777546+JasminaCasado@users.noreply.github.com>
2020-12-17 22:40:46 +0100

    Update P3.py

--- P3.py
+++ P3.py
@@ -5,7 +5,7 @@ audio = input('Nombre del audio')
 subs = input('Nombre de los subtitulos')
 output = input('Nombre del contenedor')
 
-command = 'ffmpeg ' + '-i ' + video + ' -i ' + audio + ' -i '+ subs + ' -c copy' + ' -map 0:v:0'+ ' -map 1:a:0'+ ' -c:s mov_text '+ output
+command = 'ffmpeg -i {} -i {} -i {} -c:v copy -c:a -c:s mov_text -map 0:v:0 -map 1:a:0 -map 2:s:0 {}'.format(video, audio, subs, output)
 #print(command)
 #os.system('ffmpeg', '-i', video, '-i', audio, '-i', subs, ' -c copy', ' -map 0:v:0', ' -map 1:a:0', ' -c:s mov_text', output)
 os.system(command)
